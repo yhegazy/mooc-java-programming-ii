f635b5c15299717932ea8348ce2fce1dd1a9cb2a
yahia <yhegazy@users.noreply.github.com>
2020-09-25 09:56:49 -0400

    Eighth week of updated MOOC.fi Java Course

---
+++ part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Cubes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+          
+        while(true){
+            String input = scanner.nextLine();
+        
+            if(input.equals("end")){
+                break;
+            } else {
+                System.out.println(Integer.valueOf(input) * Integer.valueOf(input) * Integer.valueOf(input));
+            }    
+        }
+    }
+}

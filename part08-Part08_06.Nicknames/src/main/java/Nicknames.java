4fab0d045e483c9c7769daee1f53b7765a353ed0
yahia <yhegazy@users.noreply.github.com>
2020-10-06 12:41:56 -0400

    Eighth week of updated MOOC.fi Java Course

---
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -0,0 +1,15 @@
+
+import java.util.HashMap;
+
+public class Nicknames {
+
+    public static void main(String[] args) {
+        // Do the operations required here!
+        
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("matthew", "matt");
+        hashmap.put("michael", "mix");
+        hashmap.put("arthur", "artie");
+    }
+
+}

56eff2d5e3ae7a90fcd49d1c8bccd3b2d7959b86
yahia <yhegazy@users.noreply.github.com>
2020-10-14 12:48:06 -0400

    Eighth week of updated MOOC.fi Java Course

---
+++ part08-Part08_13.VehicleRegistry/src/main/java/VehicleRegistry.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class VehicleRegistry {
+
+    private HashMap<LicensePlate, String> registry;
+
+    public VehicleRegistry(){
+        this.registry = new HashMap<>();
+    }
+
+    public boolean add(LicensePlate licensePlate, String owner){
+        if(!(this.registry.containsKey(licensePlate))){
+            this.registry.put(licensePlate, owner);
+            return true;
+        }
+        return false;
+    }
+
+    public String get(LicensePlate licensePlate){
+        return this.registry.get(licensePlate);
+    }
+
+    public boolean remove(LicensePlate licensePlate){
+        if(this.registry.containsKey(licensePlate)){
+            this.registry.remove(licensePlate);
+            return true;
+        }   
+        return false;
+    }
+
+    public void printLicensePlates(){
+        for(LicensePlate plate: this.registry.keySet()){
+            if(this.registry.containsKey(plate)){
+                System.out.println(registry.keySet());
+            }
+        }
+    }
+
+    public void printOwners(){
+
+        ArrayList<String> nameCheck = new ArrayList<>();
+        
+        for(String owner: this.registry.values()){
+           if(!(nameCheck.contains(owner))){
+                nameCheck.add(owner);
+                System.out.println(owner);
+            }
+        }
+    }
+}
